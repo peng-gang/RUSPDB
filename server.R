a6a20cc7dea6fb78bdcba562a78a563621693fb2
oliviazhang0416 <89941482+oliviazhang0416@users.noreply.github.com>
2021-11-25 13:59:03 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,59 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    
+shinyServer(function(input, output, session, options = options(warn = -1)) {
+  
+  ####--UI BLOCK----------------------------------------------------------------------------------------------
+  default_tab = "model"
+  
+  output$app_version <- renderUI({
+    fluidRow(
+      column(12, offset = 1, 
+             br(),
+             h5(str_c("Version ", current_version)),
+      )
+    )
+  })
+  
+  output$ui_sidebar <- renderUI({
+    sidebarMenu(id = "tab",
+                
+                menuItem("Model", 
+                         tabName = "model", 
+                         icon = icon("cube")
+                ),
+                
+                menuItem("Info", 
+                         tabName = "info", 
+                         icon = icon("question-circle")
+                ),
+                
+                uiOutput("app_version")
+    )
+  })
+  
+  output$ui_body <- renderUI({
+    updateTabsetPanel(session, "tab", selected = "model")
+    tabItems(
+      tabItem_model,
+      tabItem_info
+    )
+  })
+  
+  ####--SERVER BLOCK-----------------------------------------------------------------------------------------
+  
+  ## Constants
+  
+  ## ReactiveValues
+  v <- reactiveValues(
+    solu_slider_time = NULL
+  )
+  
+  # Server modules 
+  source('server/s_data.R', local = TRUE)
+  source('server/s_model.R', local = TRUE)
+  source('server/s_info.R', local = TRUE)
+  
+})  
 
 })
