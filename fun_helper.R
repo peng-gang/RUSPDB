9e59e9cd1e1c11314901160c9102f7a69fd4c8ea
oliviazhang0416 <89941482+oliviazhang0416@users.noreply.github.com>
2021-12-01 14:11:56 -0500

    Add files via upload

---
+++ fun_helper.R
@@ -0,0 +1,10 @@
+# Functions - common functions
+
+save_and_run <- function(){
+  rstudioapi::documentSaveAll()
+  shiny::runApp(launch.browser = TRUE)
+}
+
+# data.table approach to replace NA with zeros
+replace_nas <- function(x) { for (j in seq_len(ncol(x)))
+  set(x,which(is.na(x[[j]])),j,0) }
